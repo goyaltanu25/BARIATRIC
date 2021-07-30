171ac0c5da1ccc0dbd24ba60661ddf4a828f3d0d
Tanu Goyal <tanu.b.goyal@accenture.com>
2021-07-30 15:47:07 +0530

    changes fixes

--- views/SleepTracker.js
+++ views/SleepTracker.js
@@ -1,22 +1,61 @@
-import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
-import { View, useWindowDimensions, Image, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { View, useWindowDimensions, Dimensions, Image, TouchableOpacity, StyleSheet, Text } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 import styled from 'styled-components';
+import { BarChart } from 'react-native-chart-kit';
+
 
 import HeaderButton from '../components/HeaderButton';
 import Colors from '../constants/Colors';
 import Page from '../components/Page';
 import CardItem from '../components/CardItem';
 
-const AddButtonView = styled.View`
-display:flex;
-align-items:baseline;
-justify-content:space-around;
-margin:10px;
+const screenWidth = Dimensions.get("window").width;
+
+const data = {
+    labels: ["Week1", "Week2", "Week3", "Week4"],
+    datasets: [
+        {
+            data: [20, 40, 5, 60]
+        }
+    ]
+};
+const chartConfig = {
+    backgroundColor: '#297C75',
+    backgroundGradientFrom: "#297C75",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#297C75",
+    backgroundGradientToOpacity: 0,
+    color: (opacity = 1) => `rgba(41, 124, 117,  ${opacity})`,
+    barPercentage: 0.5,
+    style: {
+        borderRadius: 12,
+        height: 237,
+        background: 'rgba(41, 124, 117, 1)',
+    },
+    propsForDots: {
+        r: "0",
+        strokeWidth: "",
+        stroke: ""
+    },
+    propsForBackgroundLines: {
+        strokeDasharray: "",
+        strokeWidth: 0
+    },
+};
+export default function SleepTracker({ navigation }) {
+
+    const BoldText = styled.Text`
+font-style: normal;
+font-weight: bold;
+font-size: 20px;
+line-height: 20px;
+letter-spacing: 0.5px;
+color: ${Colors.textColor};
+text-align:center;
+padding-vertical:5px
 `
-const DateView = styled.View`
+    const DateView = styled.View`
 display:flex;
 align-items:baseline;
 justify-content:flex-start;
@@ -24,15 +63,8 @@ flex-direction:row;
 margin:20px;
 margin-bottom:50px;
 `
-const BoldText = styled.Text`
-font-style: normal;
-font-weight: bold;
-font-size: 20px;
-line-height: 26px;
-letter-spacing: 0.5px;
-color: ${Colors.textColor};
-`
-const CustomTabView = styled.View`
+
+    const CustomTabView = styled.View`
 display:flex;
 align-items:baseline;
 justify-content:space-between;
@@ -40,7 +72,7 @@ flex-direction:row;
 margin:20px;
 margin-bottom:50px;
 `
-const SubText = styled.Text`
+    const SubText1 = styled.Text`
 font-style: normal;
 font-weight: bold;
 font-size: 20px;
@@ -48,165 +80,143 @@ line-height: 26px;
 letter-spacing: 0.5px;
 color: ${Colors.textColor};
 `
+    const SubText = styled.Text`
+   font-style: normal;
+   font-weight: 600;
+   font-size: 16px;
+   line-height: 19px;
+   letter-spacing: 0.5px;
+   color: ${Colors.textColor};
+   text-align:center;
+   padding-vertical:5px
+ `
 
-const TimeText = styled.Text`
-font-style: normal;
-font-weight: 500;
-font-size: 12px;
-line-height: 15px;
-text-align: justify;
-color: #CF0000;
-`
-const HeadText = styled.Text`
-font-style: normal;
-font-weight: bold;
-font-size: 16px;
-line-height: 22px;
-color: #262323;
-margin-left:5%
-`
-const WrapText=styled.Text`
-color: #505050;
-margin-left:5%
-width:240px
-`
+    const WrapView = styled.View`
+  display:flex;
+  align-items:baseline;
+  flex-direction:row;
+  margin-horizontal:10px
+  `
+    const GraphView = styled.View`
+  display:flex;
+  flex-direction:row;
+  `
+    const UserName = styled.Text`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+  color: #000000;
+  margin:10px
+  `
+    const ColView = styled.View`
+  display:flex;
+  justify-content:center;
+  margin-horizontal:20%;
+  
+  `
+    const layout = useWindowDimensions();
 
-export default function SleepTracker({ navigation }) {
-  const layout = useWindowDimensions();
+    const [index, setIndex] = React.useState(0);
 
-  const [index, setIndex] = React.useState(0);
- 
-  return (
-    <Page>
-      <DateView>
-        <BoldText>13 April 2021, Tuesday</BoldText>
-      </DateView>
+    return (
+        <Page>
+            <DateView>
+                <BoldText>13 April 2021, Tuesday</BoldText>
+            </DateView>
+            <CustomTabView>
+                <SubText1>Sleep Information</SubText1>
+                <View style={{
+                    flexDirection: 'row',
+                    margin: 10,
+                    width: 87,
+                    height: 35,
+                    backgroundColor: '#CFEFE9',
+                    borderRadius: 12,
+                }}>
+                    <Text style={{ marginHorizontal: 20, marginVertical: 6, fontSize: 14, color: '#4FB6AD' }}>
+                        Change
+                    </Text>
+                </View>
+            </CustomTabView>
+            <CardItem
+                color='#DBF6E9'
+                height='110px'
+            >
+                <UserName>Jane Doe</UserName>
+                <WrapView>
+                    <Image source={require('../assets/moonsleepIcon.png')} />
+                    <Text style={{ paddingHorizontal: 10 }}>Bedtime + 7 Hrs = Wakeup</Text>
+                </WrapView>
 
-      <View>
-    <CustomTabView>
-      <SubText>Sleep Information</SubText>
-      <View style={{
-            flexDirection: 'row',
-            margin: 10,
-            width: 97,
-            height: 35,
-            backgroundColor: '#CFEFE9',
-            borderRadius: 12,
-          }}>
-            <Text style={{ margin: 8, fontSize: 14, color: '#4FB6AD' }}>
-              Calories 227
-            </Text>
-          </View>
-    </CustomTabView>
-    <CardItem
-      color='#DBF6E9'
-      height='82px'
-    >
-      <View style={{ flex: 1, flexDirection: 'row' }}>
-      <Image source={require('../assets/tablet.png')}/>
-      <WrapText style={{height:200}}>
-       <HeadText style={{flexWrap:'wrap'}}>Bupropion-naltrexone(Contrave)</HeadText>
-       <Text>
-         (5 days) 1 Tablet
-       </Text>
-      </WrapText>  
-      </View>
-      <View style={{ flex: 1, flexDirection: 'row',alignItems:'baseline',marginLeft:60,marginTop:20 ,justifyContent:'space-between'}}>
-        <Text>(Before food)</Text>
-        <TimeText>9:00 am</TimeText>
-      </View>
-    </CardItem>
-    <CardItem
-      color='#DBF6E9'
-      height='82px'
-    >
-      <View style={{ flex: 1, flexDirection: 'row' }}>
-      <Image source={require('../assets/tablet.png')}/>
-      <WrapText style={{height:200}}>
-       <HeadText style={{flexWrap:'wrap'}}>Bupropion-naltrexone(Contrave)</HeadText>
-       <Text>
-         (5 days) 1 Tablet
-       </Text>
-      </WrapText>  
-      </View>
-      <View style={{ flex: 1, flexDirection: 'row',alignItems:'baseline',marginLeft:60,marginTop:20 ,justifyContent:'space-between'}}>
-        <Text>(Before food)</Text>
-        <TimeText>9:00 am</TimeText>
-      </View>
-    </CardItem>
-    <CustomTabView>
-      <SubText>Night</SubText>
-      <Ionicons name="add" size={20} color={Colors.textColor} />
-    </CustomTabView>
-    <CardItem
-      color='#FAFAFA'
-      height='82px'
-    >
-      <View style={{ flex: 1, flexDirection: 'row' }}>
-      <Image source={require('../assets/tablet.png')}/>
-      <WrapText style={{height:200}}>
-       <HeadText style={{flexWrap:'wrap'}}>Bupropion-naltrexone(Contrave)</HeadText>
-       <Text>
-         (5 days) 1 Tablet
-       </Text>
-      </WrapText>  
-      </View>
-      <View style={{ flex: 1, flexDirection: 'row',alignItems:'baseline',marginLeft:60,marginTop:20 ,justifyContent:'space-between'}}>
-        <Text>(Before food)</Text>
-        <TimeText>9:00 am</TimeText>
-      </View>
-    </CardItem>
-    <CardItem
-      color='#FAFAFA'
-      height='82px'
-    >
-      <View style={{ flex: 1, flexDirection: 'row' }}>
-      <Image source={require('../assets/tablet.png')}/>
-      <WrapText style={{height:200}}>
-       <HeadText style={{flexWrap:'wrap'}}>Bupropion-naltrexone(Contrave)</HeadText>
-       <Text>
-         (5 days) 1 Tablet
-       </Text>
-      </WrapText>  
-      </View>
-      <View style={{ flex: 1, flexDirection: 'row',alignItems:'baseline',marginLeft:60,marginTop:20 ,justifyContent:'space-between'}}>
-        <Text>(Before food)</Text>
-        <TimeText>9:00 am</TimeText>
-      </View>
-    </CardItem>
-  </View>
-    </Page>
-  );
+            </CardItem>
+            <GraphView>
+                <CardItem
+                    color='#FCFCFC'
+                    height='169px'
+                >
+                    <ColView>
+                        <SubText>Quality</SubText>
+                        <Image source={require('../assets/Circle.png')} />
+                        <SubText>90%</SubText>
+                    </ColView>
+                </CardItem>
+                <CardItem
+                    color='#FCFCFC'
+                    height='169px'
+                >
+                    <ColView>
+                        <SubText>Duration</SubText>
+                        <Image source={require('../assets/Circle.png')} />
+                        <SubText>7 hrs</SubText>
+                    </ColView>
+                </CardItem>
+            </GraphView>
+            <CustomTabView>
+                <SubText1>Average Sleep - 45 hrs a Week</SubText1>
+            </CustomTabView>
+            <CardItem
+                color='#DBF6E9'
+                height='280px'
+            >
+                <BarChart
+                    data={data}
+                    width={screenWidth}
+                    height={220}
+                    chartConfig={chartConfig}
+
+                />
+            </CardItem>
+        </Page>
+    );
 }
 
 // Header Navigation
 SleepTracker.navigationOptions = ({ navigation }) => {
-  return {
-    headerTitle: 'Sleep Tracker',
-    headerLeft: () =>
-      <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item
-          iconName="chevron-back-outline"
-          onPress={() => {
-            navigation.goBack();
-          }}
-        />
-      </HeaderButtons>,
-    headerRight: () =>
-      <AddButtonView>
-        <TouchableOpacity activeOpacity={0.7} onPress={() => navigation.navigate('AddReminder')}>
-          <Image source={require('../assets/success 2.png')}/>
-        </TouchableOpacity >
-      </AddButtonView>,
+    return {
+        headerTitle: 'Sleep Tracker',
+        headerLeft: () =>
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    iconName="chevron-back-outline"
+                    onPress={() => {
+                        navigation.goBack();
+                    }}
+                />
+            </HeaderButtons>,
+        headerRight: () =>
+            <TouchableOpacity activeOpacity={0.7} onPress={() => navigation.navigate('AddReminder')}>
+                <Image source={require('../assets/success 2.png')} />
+            </TouchableOpacity >
 
-  };
+    };
 }
 
 const styles = StyleSheet.create({
-  activeTabTextColor: {
-    color: 'black'
-  },
-  tabTextColor: {
-    color: 'black'
-  }
+    activeTabTextColor: {
+        color: 'black'
+    },
+    tabTextColor: {
+        color: 'black'
+    }
 })
